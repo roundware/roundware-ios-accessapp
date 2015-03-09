76b3c9fccd9ce20452cf0e1592e95d5a9ced8af4
Parveen Kaler <pk@smartfulstudios.com>
2015-03-09 15:24:59 -0700

    Contribute artifact view controller

---
+++ Digita11y/Controllers/ContributeArtifactTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContributeArtifactTableViewController.swift
+//  Digita11y
+//
+//  Created by Parveen Kaler on 2015-03-09.
+//  Copyright (c) 2015 Roundware. All rights reserved.
+//
+
+import UIKit
+
+class ContributeArtifactTableViewController: UITableViewController {
+  
+  var cells = ["Lovell Pressure Suit", "Telescope M-53", "Apollo XVII Moon Rock", "Apollo XIII Manual"]
+
+  // MARK: - Table view data source
+
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return self.cells.count
+  }
+
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("ContributeArtifactCellIdentifier", forIndexPath: indexPath) as UITableViewCell
+    cell.textLabel?.text = self.cells[indexPath.row]
+    return cell
+  }
+}
