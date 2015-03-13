52b315cc2def727e6103fecae5eea163aa56dcce
Parveen Kaler <pk@smartfulstudios.com>
2015-03-12 20:25:38 -0700

    Hook up RoundwareFramework

--- Digita11y/AppDelegate.swift
+++ Digita11y/AppDelegate.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import RWFramework
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, RWFrameworkProtocol {
 
   var window: UIWindow?
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: false)
+
+    var rwf = RWFramework.sharedInstance
+    rwf.delegate = self
+    rwf.start()
+
     return true
+  }
+
+  func rwUpdateStatus(message: String) {
+    debugPrintln(message)
+  }
+
+  func rwPostUsersFailure(error: NSError?) {
+    debugPrintln(error)
   }
 }
