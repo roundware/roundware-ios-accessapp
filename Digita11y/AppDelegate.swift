04fa7475a3be580e30720775a06e092a503e2f6d
Parveen Kaler <pk@smartfulstudios.com>
2015-03-28 16:26:43 -0700

    Save off listen/speak tags

--- Digita11y/AppDelegate.swift
+++ Digita11y/AppDelegate.swift
@@ -12,8 +12,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate, RWFrameworkProtocol {
     Crashlytics.startWithAPIKey("69056dd4dfd70d4a7ca049983df384d1c090537f")
     UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: false)
 
-    rwData = RWData()
+    setupRWFramework()
 
     return true
   }
+
+  func setupRWFramework() {
+    rwData = RWData()
+    var rwf = RWFramework.sharedInstance
+    rwf.addDelegate(self)
+    rwf.start()
+  }
+
+  func rwUpdateStatus(message: String) {
+    //debugPrintln(message)
+  }
+
+  func rwPostUsersFailure(error: NSError?) {
+    debugPrintln(error)
+  }
+
+  func rwPostSessionsSuccess() {
+  }
+
+  func rwGetProjectsIdTagsSuccess(data: NSData?) {
+    let json = JSON(data: data!)
+    self.rwData?.speakTags = json["speak"].array?.map { TagGroup(json: $0) } ?? []
+    self.rwData?.listenTags = json["listen"].array?.map { TagGroup(json: $0) } ?? []
+    debugPrintln(self.rwData?.speakTags)
+    debugPrintln(self.rwData?.listenTags)
+  }
+
+  func rwGetProjectsIdTagsFailure(error: NSError?) {
+    debugPrintln(error)
+  }
+
 }
