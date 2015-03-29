c840fb4b82447a669a5b6622e33e27b0d39f3dfe
Parveen Kaler <pk@smartfulstudios.com>
2015-03-28 17:23:32 -0700

    Display speak tags from API

--- Digita11y/Controllers/BaseViewController.swift
+++ Digita11y/Controllers/BaseViewController.swift
@@ -2,4 +2,10 @@ import UIKit
 
 class BaseViewController: UITableViewController {
   var rwData: RWData?
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if let to = segue.destinationViewController as? BaseViewController {
+      to.rwData = self.rwData
+    }
+  }
 }
