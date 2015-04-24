7ec4161e4a38b3b8bd5a9bce61f63c0b534d4f98
Parveen Kaler <pk@smartfulstudios.com>
2015-04-23 19:31:24 -0700

    An All Channels selection for listen tags

--- Digita11y/Controllers/ListenTagsTableViewController.swift
+++ Digita11y/Controllers/ListenTagsTableViewController.swift
@@ -16,7 +16,12 @@ class ListenTagsTableViewController: BaseTableViewController {
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return self.rwData?.listenTags[section].options.count ?? 0
+    let count = self.rwData?.listenTags[section].options.count ?? 0
+    if self.rwData?.listenTags[section].code == "channel" {
+      return count + 1
+    }
+
+    return count
   }
 
   override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -25,14 +30,42 @@ class ListenTagsTableViewController: BaseTableViewController {
 
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "ListenTagsCellIdentifier")
-    if let tag = self.rwData?.listenTags[indexPath.section].options[indexPath.row] {
-      cell.textLabel?.text = tag.value
-      let i = indexPath.row + 1
-      let count = self.rwData?.listenTags[indexPath.section].options.count ?? 0
-      cell.accessibilityLabel = String("\(tag.value), \(i) of \(count)")
-      cell.accessibilityTraits = UIAccessibilityTraitButton
 
-      var rwf = RWFramework.sharedInstance
+    var rwf = RWFramework.sharedInstance
+    let i = indexPath.row + 1
+    var count = self.rwData?.listenTags[indexPath.section].options.count ?? 0
+    if self.rwData?.listenTags[indexPath.section].code == "channel" {
+      if self.rwData?.listenTags[indexPath.section].options.count == indexPath.row {
+        cell.textLabel?.text = "All Channels"
+
+        if let tags = rwf.getListenTagsCurrent("channel") as! NSArray? {
+          cell.accessoryType = (tags.count == count) ? .Checkmark : .None
+        }
+      } else if let tag = self.rwData?.listenTags[indexPath.section].options[indexPath.row] {
+        cell.textLabel?.text = tag.value
+
+        if let tags = rwf.getListenTagsCurrent("channel") as! NSArray? {
+          if tags.count == count {
+            cell.accessoryType = .None
+          } else {
+            cell.accessoryType = .None
+            for tagID in tags {
+              if let tagID = tagID as? Int {
+                if tagID == tag.tagId {
+                  cell.accessoryType = .Checkmark
+                  break
+                }
+              }
+            }
+          }
+        }
+      }
+
+      ++count
+
+    } else if let tag = self.rwData?.listenTags[indexPath.section].options[indexPath.row] {
+      cell.textLabel?.text = tag.value
+
       if let tags = rwf.getAllListenTagsCurrent() as! NSArray? {
         cell.accessoryType = .None
         for tagID in tags {
@@ -46,13 +79,33 @@ class ListenTagsTableViewController: BaseTableViewController {
       }
     }
 
+    cell.accessibilityLabel = String("\(cell.textLabel?.text), \(i) of \(count)")
+    cell.accessibilityTraits = UIAccessibilityTraitButton
+
     return cell
   }
 
   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    if let group = self.rwData?.listenTags[indexPath.section],
-            cell = tableView.cellForRowAtIndexPath(indexPath) {
-      var rwf = RWFramework.sharedInstance
+
+    var rwf = RWFramework.sharedInstance
+
+    if self.rwData?.listenTags[indexPath.section].code == "channel" {
+      if self.rwData?.listenTags[indexPath.section].options.count == indexPath.row {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath), group = self.rwData?.listenTags[indexPath.section] {
+          cell.accessoryType = .Checkmark
+          let tags = rwf.getListenTagsCurrent(group.code) as? [Int] ?? []
+          rwf.setListenTagsCurrent("channel", value: tags)
+          cell.selected = false
+        }
+      } else {
+        if let cell = tableView.cellForRowAtIndexPath(indexPath), group = self.rwData?.listenTags[indexPath.section] {
+          let tag = group.options[indexPath.row]
+          rwf.setListenTagsCurrent("channel", value: [tag.tagId])
+          cell.selected = false
+        }
+      }
+    } else if let group = self.rwData?.listenTags[indexPath.section],
+                   cell = tableView.cellForRowAtIndexPath(indexPath) {
       if let tags = rwf.getListenTagsCurrent(group.code) as? [Int] {
         let tag = group.options[indexPath.row]
         var newTags = tags.filter { $0 == tag.tagId }
