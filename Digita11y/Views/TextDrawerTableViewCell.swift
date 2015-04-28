17431c6ad564228bca065b89d9bdb0cae3fabec5
Parveen Kaler <pk@smartfulstudios.com>
2015-04-28 13:24:50 -0700

    Clean up contribute table view cells

--- Digita11y/Views/TextDrawerTableViewCell.swift
+++ Digita11y/Views/TextDrawerTableViewCell.swift
@@ -1,5 +1,16 @@
 import UIKit
 
 class TextDrawerTableViewCell: UITableViewCell {
+
+  static let Identifier  = "TextDrawerCellIdentifier"
+
   @IBOutlet weak var contributeTextView: SZTextView!
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+    contributeTextView.placeholder = "Write something..."
+    contributeTextView.placeholderTextColor = UIColor.lightGrayColor()
+    contributeTextView.returnKeyType = .Done
+  }
 }
